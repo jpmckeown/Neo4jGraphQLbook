2840f87140f1ec9f3d333b74d0ab110551e54b41
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2023-02-13 14:58:22 +0000

    form code moved to BusinessResults.js

--- web-react/src/App.js
+++ web-react/src/App.js
@@ -1,3 +1,5 @@
+
+import BusinessResults from './BusinessResults';
 import logo from './logo.svg';
 import './App.css';
 
@@ -38,25 +40,7 @@ function App() {
       </label>
       <input type="submit" value="Submit" />
    </form>
-   <h2>Results</h2>
-   <table>
-   <thead>
-      <tr>
-      <th>Name</th>
-      <th>Address</th>
-      <th>Category</th>
-      </tr>
-   </thead>
-   <tbody>
-   {businesses.map((b, i) => (
-   <tr key={i}>
-   <td>{b.name}</td>
-   <td>{b.address}</td>
-   <td>{b.category}</td>
-   </tr>
-   ))}
-   </tbody>
-   </table>
+   <BusinessResults businesses={businesses} />       
    </div>
    );
 }
