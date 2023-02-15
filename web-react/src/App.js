b910837bb198c2b8970dc3c18e39982b813ec967
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2023-02-15 07:47:03 +0000

    re-integrated business search into App.js because book ignores ch5 exercise

--- web-react/src/App.js
+++ web-react/src/App.js
@@ -1,16 +1,47 @@
-import React from "react";
-//import React, {useState} from "react";
-//import BusinessResults from './BusinessResults';
-import BusinessSearch from './BusinessSearch'
-import {gql, useQuery} from "@apollo/client"
+import React, {useState} from "react";
+import BusinessResults from './BusinessResults';
+import {gql, useQuery} from "@apollo/client";
 import './App.css';
+// import BusinessSearch from "./BusinessSearch";
+// import React from "react";
 
 const GET_BUSINESSES_QUERY = gql`
+   businesses {
+      businessId
+      name
+      address
+      categories{
+         name
+      }
+   }
 `;
 
 function App() {
+   const [selectedCategory, setSelectedCategory] = useState("All");
+   const {loading, error, data} = useQuery(GET_BUSINESSES_QUERY);
+
    return (
-   <BusinessSearch />
+      <div>
+      <h1>Business Search</h1>
+      <form>
+      <label>Select Business Category:
+
+      <select 
+         value={selectedCategory}
+         onChange={(event) => setSelectedCategory(event.target.value)}
+      >
+         <option value="All">All</option>
+         <option value="Library">Library</option>
+         <option value="Restaurant">Restaurant</option>
+         <option value="Car Wash">Car Wash</option>
+               </select>
+               
+      </label>
+            <input type="submit" value="Submit" />
+      </form>
+         
+         <BusinessResults businesses={data.businesses} /> 
+      </div>
    );
 }
 
