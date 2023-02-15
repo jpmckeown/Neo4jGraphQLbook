f592e9f443268dfdf496c8d336f770aba525f959
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2023-02-15 07:10:27 +0000

    chapter 6 begun, Apollo client setup

--- web-react/src/App.js
+++ web-react/src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 //import React, {useState} from "react";
 //import BusinessResults from './BusinessResults';
 import BusinessSearch from './BusinessSearch'
+import {gql, useQuery} from "@apollo/client"
 import './App.css';
+
+const GET_BUSINESSES_QUERY = gql`
+`;
 
 function App() {
    return (
